17ae8e0c5791665c2f1f31b751a8f2b71016baa5
ImineFarouk <faroukimine@gmail.com>
2025-06-18 14:37:00 +0100

    Initial commit - Spring Boot DEMO BANKING APP

---
+++ src/main/java/com/imine/farouk/FaroukApplication.java
@@ -0,0 +1,13 @@
+package com.imine.farouk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaroukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaroukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imine/farouk/FaroukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imine.farouk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaroukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
